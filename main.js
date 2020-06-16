1be4071609319230495d7c3995bf7201af8cbea2
noan <wauchikesousa@yahoo.com>
2020-06-16 17:57:38 +0200

    make it better

--- main.js
+++ main.js
@@ -225,7 +225,7 @@
       .setTitle(`c'est l'avatar de ${message.author.username}`)
       
       .setColor('#00FFFF')
-     .setImage(message.author.displayAvatarURL('jpg', 512));
+     .setImage(message.author.displayAvatarURL('jpg', '512x512'));
       message.channel.send(embedA)
     }
     //l'avatar deffinit pour la suite

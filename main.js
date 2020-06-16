d1659e11e52161b09d417fb3773e4be5775b5911
noan <wauchikesousa@yahoo.com>
2020-06-16 13:08:34 +0200

    make it better

--- main.js
+++ main.js
@@ -223,7 +223,7 @@
       //ton avatar
       let embedA = new Discord.MessageEmbed()
       .spliceFields(25)
-      .addField(message.author.displayAvatarURL());
+      .addimage(message.author.displayAvatarURL('jpg', 64));
       message.channel.send(embedA)
     }
     //l'avatar deffinit pour la suite
